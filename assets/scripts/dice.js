566e6242b13957f9d456da62fba692aaeecdc030
meataxe <mark.bellhouse@gmail.com>
2016-04-29 11:23:40 +1200

    Update dice.js
    js changes

--- assets/scripts/dice.js
+++ assets/scripts/dice.js
@@ -1,101 +1,56 @@
-var dice = [
-  { 
-    number: 1, 
-    faces: [
-      { title: "lightbulb", class: "flaticon-technology" },
-      { title: "arrow (bow)", class: "flaticon-weapon" },
-      { title: "sleepy face", class: "flaticon-people" },
-      { title: "house", class: "flaticon-home" },
-      { title: "clock", class: "flaticon-clock" },
-      { title: "speechbubble", class: "flaticon-multimedia" }
-    ]
-  },
-  { 
-    number: 2, 
-    faces: [
-      { title: "happy face", class: "flaticon-people-1" },
-      { title: "globe", class: "flaticon-globe" },
-      { title: "office block", class: "flaticon-city" },
-      { title: "plane", class: "flaticon-transport" },
-      { title: "apple", class: "flaticon-fruit" },
-      { title: "torch", class: "flaticon-light" }
-    ]
-  },  
-  { 
-    number: 3, 
-    faces: [
-      { title: "tree", class: "flaticon-nature" },
-      { title: "postcard", class: "flaticon-back" },
-      { title: "pyramid", class: "flaticon-monuments" },
-      { title: "river", class: "flaticon-nature-1" },
-      { title: "castle tower", class: "flaticon-summer" },
-      { title: "eye", class: "flaticon-view" }      
-    ]
-  },  
-  { 
-    number: 4, 
-    faces: [
-      { title: "magic wand", class: "flaticon-witch" },
-      { title: "bridge over stream", class: "flaticon-road" },
-      { title: "bumblebee", class: "flaticon-animals" },
-      { title: "abacus", class: "flaticon-tool" },
-      { title: "book", class: "flaticon-book" },
-      { title: "moon", class: "flaticon-moon" }     
-    ]
-  },  
-  { 
-    number: 5, 
-    faces: [
-      { title: "hand", class: "flaticon-stop" },
-      { title: "beetle", class: "flaticon-animal" },
-      { title: "turtle", class: "flaticon-animal-1" },
-      { title: "dragon", class: "flaticon-animals-2" },
-      { title: "dice", class: "flaticon-game" },
-      { title: "magnifying glass", class: "flaticon-search" }
-    ]
-  },  
-  { 
-    number: 6, 
-    faces: [
-      { title: "sheep", class: "flaticon-animals-3" },
-      { title: "footprint", class: "flaticon-shape" },
-      { title: "magnet", class: "flaticon-tool-1" },
-      { title: "padlock", class: "flaticon-lock" },
-      { title: "bat", class: "flaticon-animals-1" },
-      { title: "ghost", class: "flaticon-creepy" }      
-    ]
-  },  
-  { 
-    number: 7, 
-    faces: [
-      { title: "fish", class: "flaticon-food" },
-      { title: "keyhole", class: "flaticon-key" },
-      { title: "compas points", class: "flaticon-compass" },
-      { title: "flower", class: "flaticon-nature-2" },
-      { title: "parachute", class: "flaticon-paratrooper" },
-      { title: "happy/sad faces", class: "flaticon-happy-and-sad-masks" }      
-    ]
-  },  
-  { 
-    number: 8, 
-    faces: [
-      { title: "shooting star", class: "flaticon-hand-drawn-shooting-star" },
-      { title: "questionmark", class: "flaticon-question" },
-      { title: "fountain", class: "flaticon-fountain" },
-      { title: "sad face", class: "flaticon-boy-with-sadness-face" },
-      { title: "key", class: "flaticon-key-1" },
-      { title: "tent", class: "flaticon-tent" }      
-    ]
-  },  
-  { 
-    number: 9, 
-    faces: [
-      { title: "phone", class: "flaticon-phone-call" },
-      { title: "scales", class: "flaticon-scales-of-justice" },
-      { title: "arrow (direction)", class: "flaticon2-right-arrow" },
-      { title: "lightning", class: "flaticon2-storm" },
-      { title: "walking cane", class: "flaticon2-hiker" },
-      { title: "alien", class: "flaticon2-ufo" }
-    ]
-  }
+var images = [
+  { title: "lightbulb",         class: "flaticon-technology" },
+  { title: "arrow (bow)",       class: "flaticon-weapon" },
+  { title: "sleepy face",       class: "flaticon-people" },
+  { title: "house",             class: "flaticon-home" },
+  { title: "clock",             class: "flaticon-clock" },
+  { title: "speechbubble",      class: "flaticon-multimedia" },
+  { title: "happy face",        class: "flaticon-people-1" },
+  { title: "globe",             class: "flaticon-globe" },
+  { title: "office block",      class: "flaticon-city" },
+  { title: "plane",             class: "flaticon-transport" },
+  { title: "apple",             class: "flaticon-fruit" },
+  { title: "torch",             class: "flaticon-light" },
+  { title: "tree",              class: "flaticon-nature" },
+  { title: "postcard",          class: "flaticon-back" },
+  { title: "pyramid",           class: "flaticon-monuments" },
+  { title: "river",             class: "flaticon-nature-1" },
+  { title: "castle tower",      class: "flaticon-summer" },
+  { title: "eye",               class: "flaticon-view" },      
+  { title: "magic wand",        class: "flaticon-witch" },
+  { title: "bridge over stream",class: "flaticon-road" },
+  { title: "bumblebee",         class: "flaticon-animals" },
+  { title: "abacus",            class: "flaticon-tool" },
+  { title: "book",              class: "flaticon-book" },
+  { title: "moon",              class: "flaticon-moon" },     
+  { title: "hand",              class: "flaticon-stop" },
+  { title: "beetle",            class: "flaticon-animal" },
+  { title: "turtle",            class: "flaticon-animal-1" },
+  { title: "dragon",            class: "flaticon-animals-2" },
+  { title: "dice",              class: "flaticon-game" },
+  { title: "magnifying glass",  class: "flaticon-search" },
+  { title: "sheep",             class: "flaticon-animals-3" },
+  { title: "footprint",         class: "flaticon-shape" },
+  { title: "magnet",            class: "flaticon-tool-1" },
+  { title: "padlock",           class: "flaticon-lock" },
+  { title: "bat",               class: "flaticon-animals-1" },
+  { title: "ghost",             class: "flaticon-creepy" },      
+  { title: "fish",              class: "flaticon-food" },
+  { title: "keyhole",           class: "flaticon-key" },
+  { title: "compas points",     class: "flaticon-compass" },
+  { title: "flower",            class: "flaticon-nature-2" },
+  { title: "parachute",         class: "flaticon-paratrooper" },
+  { title: "happy/sad faces",   class: "flaticon-happy-and-sad-masks" },      
+  { title: "shooting star",     class: "flaticon-hand-drawn-shooting-star" },
+  { title: "questionmark",      class: "flaticon-question" },
+  { title: "fountain",          class: "flaticon-fountain" },
+  { title: "sad face",          class: "flaticon-boy-with-sadness-face" },
+  { title: "key",               class: "flaticon-key-1" },
+  { title: "tent",              class: "flaticon-tent" },    
+  { title: "phone",             class: "flaticon-phone-call" },
+  { title: "scales",            class: "flaticon-scales-of-justice" },
+  { title: "arrow (direction)", class: "flaticon2-right-arrow" },
+  { title: "lightning",         class: "flaticon2-storm" },
+  { title: "walking cane",      class: "flaticon2-hiker" },
+  { title: "alien",             class: "flaticon2-ufo" }    
 ];
